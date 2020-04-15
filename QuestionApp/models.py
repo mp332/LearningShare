a24542f63d4e361d37a58508c2357e5bb59c8cfb
dingyuandy <46886037+dingyuandy@users.noreply.github.com>
2020-04-15 22:44:11 +0800

    Add files via upload

---
+++ QuestionApp/models.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40, unique=True, verbose_name='版块名称')
+    number = models.IntegerField(unique=True, verbose_name='板块序号')
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, verbose_name='提问者', related_name='questions',on_delete=models.CASCADE)
+    questionTitle = models.CharField('问题标题', max_length=40, unique=True,blank=False)
+    questionCategory = models.ForeignKey('Category', verbose_name='板块名称', on_delete=models.CASCADE)
+    questionDescription = models.TextField('详细描述')
+    publishDate = models.DateTimeField(auto_now=True, verbose_name='发布日期')
+    godNum = models.IntegerField(default=0,verbose_name='赞数')
+    badNum = models.IntegerField(default=0, verbose_name='反对数')
+    grade = models.IntegerField(default=0, verbose_name='综合质量')
+    collect = models.ManyToManyField(User, verbose_name='收藏')
+
+    def __str__(self):
+        return self.questionTitle
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,verbose_name='评论者',on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', related_name='comments', on_delete=models.CASCADE)
+    commentText = models.TextField(verbose_name='评论内容')
+    pubDate = models.DateTimeField(auto_now=True, verbose_name='回答时间')
+    goodNum = models.IntegerField(default=0)
+    badNum = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.commentText
