bf7b5f2fad88ac6b501c757fbe579f8d3cf3239c
P Mr <mr.p332@sjtu.edu.cn>
2020-04-22 15:53:56 +0800

    初始化配置

--- LearningShare/settings.py
+++ LearningShare/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AnswerApp',
+    'QuestionApp',
+    'CategoryApp',
+    'AccountApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'LearningShare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':[os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
