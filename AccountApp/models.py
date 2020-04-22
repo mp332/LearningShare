3e2a646d840a385ac29e5ce6ed1dcebd9fe7e6b6
P Mr <mr.p332@sjtu.edu.cn>
2020-04-22 21:30:01 +0800

    重新配置数据库，注册可用

--- AccountApp/models.py
+++ AccountApp/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
-class UserProfile(models.Model):#在数据库中建立的名为account_userprofil 的数据库表。
+class UserProfile(models.Model):  # 在数据库中建立的名为account_userprofil 的数据库表。
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
-    birth = models.DateField(blank=True, null=True)   # DateField 格式必须为xxxx-xx-xx
+    birth = models.DateField(blank=True, null=True)  # DateField 格式必须为xxxx-xx-xx
     phone = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return 'user {}'.format(self.user.username)
+
 
 '''class UserInfo(models.Model):
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
@@ -16,5 +21,4 @@ class UserProfile(models.Model):#在数据库中建立的名为account_userprofi
     address = models.CharField(max_length=100, blank=True)
     aboutme = models.TextField(blank=True)
 
-    def __str__(self):
-        return 'user {}'.format(self.user.username)'''
+    '''
