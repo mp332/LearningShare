bb82ddf9308f1951090deb5822da551900b3269f
lzm08042 <61974955+lzm08042@users.noreply.github.com>
2020-04-22 14:01:05 +0800

    Add files via upload

---
+++ AccountApp/urls.py
@@ -0,0 +1,39 @@
+#from django.contrib.auth.views import LoginView
+from django.conf.urls import url
+from .import views
+
+from django.conf import settings
+
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LoginView
+
+app_name = 'AccountApp'
+
+urlpatterns = [
+    #url(r'^login/$', views.user_login, name="user_login"),
+    url('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name="user_login"),
+    url("new-login/", auth_views.LoginView.as_view, {"template_name": "account/login.html"}),
+    url('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name="user_logout"),
+    url("logout/", auth_views.LoginView.as_view, {"template_name": "account/logout.html"}, name="user_logout"),
+    url(r'^register/$', views.register, name="user_register"),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='account/password_change_form.html'),
+         {"post_change_redirect": "registration/password-change-done"}, name='password_change'),
+    path('password-change-done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'),
+         name='password_change_done'),
+    url(r'^password-reset/$', auth_views.PasswordResetView.as_view(template_name="account/password_reset_form.html", success_url=reverse_lazy('account:password_reset_done'),
+        email_template_name='account/password_reset_email.html', subject_template_name='account/password_reset_subject.txt'),  name="password_reset"),
+    url(r'^password-reset-done/$', auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_done.html"),
+        name="password_reset_done"),
+    url(r'^password-reset-confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html", success_url=reverse_lazy('account:password_reset_complete')),
+        name="password_reset_confirm"),
+    url(r'^password-reset-complete/$', auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_complete.html"),
+        name="password_reset_complete"),
+
+    url(r'^my-information/$', views.myself, name="my_information"),
+    url(r'^edit-my-information/$', views.myself_edit, name="edit_my_information"),
+
+
+]
---
+++ AccountApp/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login  #1从 Django 默认的（或者说是内置的）用户认证和管理应用中引入的两个方法。
+from .forms import LoginForm, RegistrationForm, UserProfileForm, UserInfoForm, UserForm
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile, UserInfo
+from django.contrib.auth.models import User
+
+# Create your views here.
+def user_login(request):  #2视图函数中要处理前端提交的数据，井支持前端的显示请求
+    if request.method == "POST":  #3返回 HTTP请求类型的字符串
+        login_form = LoginForm(request.POST)   #4前端浏览器向服务器端提交表单内容
+        if login_form.is_valid():   #5验证所传入的数据是否合法
+            cd = login_form.cleaned_data    #6cd引用的是一个字典类型数据，其中以键值对的形式记录了用户名和密码。
+            user = authenticate(username=cd['username'], password=cd['password'])  #7authenticate（）函数，其作用是检验此用户是否为本网站项目的用户,以及其密码是否正确.如果都对上号，就返回User的一个实例对象，否则返回None
+            if user:
+                login(request, user)   #8login（）函数，以语句⑦所得到的User实例对象作为参数，实现用户登录
+                return HttpResponse("Wellcom You. You have been authenticated successfully")   #9
+            else:
+                return HttpResponse("Sorry. Your username or password is not right.")
+        else:
+            return HttpResponse("Invalid login")
+    if request.method == "GET":
+        login_form = LoginForm()
+        return render(request, "account/login.html", {"form":login_form})
+
+def register(request):
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
+            UserInfo.objects.create(user=new_user)
+            return HttpResponse("successfully")
+        else:
+            return HttpResponse("sorry,your can not register.")
+    else:
+        user_form = RegistrationForm()
+        userprofile_form = UserProfileForm()
+        return render(request, "account/register.html", {"form": user_form, "profile":userprofile_form})
+
+@login_required(login_url='/account/login/')
+def myself(request):
+    user = User.objects.get(username=request.user.username)
+    userprofile = UserProfile.objects.get(user=user)
+    userinfo = UserInfo.objects.get(user=user)
+    return render(request, "account/myself.html", {"user": user, "userinfo": userinfo, "userprofile": userprofile})
+
+@login_required(login_url='/account/login/')
+def myself_edit(request):
+    user = User.objects.get(username=request.user.username)
+    userprofile = UserProfile.objects.get(user=request.user)
+    userinfo = UserInfo.objects.get(user=request.user)
+
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        userinfo_form = UserInfoForm(request.POST)
+        if user_form.is_valid()*userprofile_form.is_valid()*userinfo_form.is_valid():
+            user_cd = user_form.cleaned_data
+            userprofile_cd = userprofile_form.cleaned_data
+            userinfo_cd = userinfo_form.cleaned_data
+            print(user_cd["email"])
+            user.email = user_cd['email']
+            userprofile.birth = userprofile_cd['birth']
+            userprofile.phone = userprofile_cd['phone']
+            userinfo.school = userinfo_cd['school']
+            userinfo.company = userinfo_cd['company']
+            userinfo.profession = userinfo_cd['profession']
+            userinfo.address = userinfo_cd['address']
+            userinfo.aboutme = userinfo_cd['aboutme']
+            user.save()
+            userprofile.save()
+            userinfo.save()
+            return HttpResponseRedirect('/account/my-information/')
+        else:
+            user_form = UserForm(instance=request.user)
+            userprofile_form = UserProfileForm(initial={"birth":userprofile.birth, "phone":userprofile.phone})
+            userinfo_form = UserInfoForm(initial={"school":userinfo.school, "company":userinfo.company, "profession":userinfo.profession, "address":userinfo.address, "aboutme":userinfo.aboutme})
+            return render(request, "account/myself_edit.html", {"user_form":user_form, "userprofile_form":userprofile_form, "userinfo_form":userinfo_form})
+
+
+
+
+
+
