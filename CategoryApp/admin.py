989cd876ba8678c75c17d180d9ecca4938a4ac53
P Mr <mr.p332@sjtu.edu.cn>
2020-04-22 21:33:33 +0800

    更新后台管理

--- CategoryApp/admin.py
+++ CategoryApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category
+
+
+# from .models import category
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "number")
+    list_filter = ("number",)
 
 # Register your models here.
