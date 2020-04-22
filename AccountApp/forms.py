940ccd905aa5e728682bcc3c7a74b48987bd4588
lzm08042 <61974955+lzm08042@users.noreply.github.com>
2020-04-22 13:59:50 +0800

    Add files via upload

---
+++ AccountApp/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.models import User   #引入了Django 默认的用户模型 User 类
+from .models import UserProfile, UserInfo
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("passwords do not match.")
+        return cd['password2']
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ("phone", "birth")
+
+'''class UserInfoForm(forms.ModelForm):
+    class Meta:
+        model = UserInfo
+        fields = ("school", "company", "profession", "address", "aboutme")
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email",)'''
