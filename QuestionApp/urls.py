e71908e33ce202df1b72cac07f4d3148a42a915b
P Mr <mr.p332@sjtu.edu.cn>
2020-04-22 15:56:30 +0800

    添加地址配置文件

---
+++ QuestionApp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.views.generic import RedirectView
+from django.conf.urls import url
+from . import views
+
+app_name = 'question'
+
+urlpatterns = [
+    # path('index/',views.index,name='index'),
+    # path('', RedirectView.as_view(url='index/')),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.question_content, name="question_content"),
+    # path('add_question',views.add_question,name='add_question')
+    path('add_question/', views.ask, name='add_question'),
+
+]
