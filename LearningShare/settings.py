42192ff5624458dbbf57f47ec7d150e1715c1b9f
dy <dingyuanlanting@outlook.com>
2020-05-27 18:18:30 +0800

    完成分页

--- LearningShare/settings.py
+++ LearningShare/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-LOGIN_REDIRECT_URL = '/question/1'
+LOGIN_REDIRECT_URL = '/question/question_index/1'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
